e7bd557a1be026aa8e0559fb458c2a2201f6bd61
Harryjeffs <harry.jeffs2222@gmail.com>
2019-11-25 13:02:47 +1100

    Added image upload, network completion handler & json data for database input.

--- Dating Space/Controllers/Tab Bar Views/New Post/Support Group/Class/newPost.swift
+++ Dating Space/Controllers/Tab Bar Views/New Post/Support Group/Class/newPost.swift
@@ -8,44 +8,76 @@
 
 import Foundation
 import Firebase
+import FirebaseStorage
 
 class newPost {
     
     var db : Firestore!
     var documentID: String = ""
+
+    let storageRef = StorageReference()
+    var imagePathArray = [String]()
     
-    func createDatabase(_ success: @escaping(_: Bool) -> Void ){
+    func createInitalPost(_ success: @escaping(_: Bool) -> Void ){
         db = Firestore.firestore()
-        self.db.collection("users").document(UUID().uuidString).setData([
-            "friends": [
-                "name": "none",
-                "uid" : "none"
-            ],
-            "friend_count" : 0,
-            "uuid": "auth.user.uid",
-            "display_name": "auth.user.displayName as Any",
-            "user_data": "auth.additionalUserInfo?.profile as Any",
-            "admin" : false,
-            "user_settings" : [
-                "banned": false,
-                "looking_for": "male",
-                "location": [
-                    "lat" : userDetails.data.currentLocation.coordinate.latitude,
-                    "lng" : userDetails.data.currentLocation.coordinate.longitude
-                ]
-            ]
-        ]){ err in
-            if let _ = err{
-                //EZAlertController - alert we couldnt register the user.
-                success(false)
-            }else{
-                //Perform a segue to the home screen
-                //Assign user.uniqueID = auth.user.uid
-                //userDetails.data.uniqueID = auth.user.uid
-                //self.generateUserData()
-                success(true)
+        
+        let postUID = UUID().uuidString
+        imageUpload(postUID, completion: { imagesArray in
+            self.db.collection("users").document(postUID).setData([
+                "age" : 0,
+                "cons": onboaringData.cons,
+                "pros": onboaringData.pros,
+                "socials": onboaringData.socials,
+                "description": onboaringData.description,
+                "gender": onboaringData.gender,
+                "images": imagesArray,
+                "admin" : false,
+                "status" : [
+                    "approved": 0,
+                    "approving_user": "/users/iefdbkbqwurh239",
+                ],
+                "post_uid": postUID,
+                "timestamp": ServerValue.timestamp(),
+                "deleted": false,
+                "posted_by": "/users/\(userDetails.data.uniqueID)",
+            ]){ err in
+                if let error = err{
+                    //EZAlertController - alert we couldnt register the user.
+                    print(error)
+                    success(false)
+                }else{
+                    print("Successfully uploaded \(postUID)")
+                    //Perform a segue to the home screen
+                    //Assign user.uniqueID = auth.user.uid
+                    //userDetails.data.uniqueID = auth.user.uid
+                    //self.generateUserData()
+                    success(true)
+                }
+            }
+            //return documentID
+        })
+    }
+    func imageUpload(_ postId: String, completion: @escaping (_ imagesArray: [String]) -> Void){
+        let dispatchGroup = DispatchGroup()
+        
+        for singleImage in onboaringData.photos{
+            let imageId = UUID().uuidString
+            
+            let imagePath = storageRef.child(postId + "/" + imageId + ".png")
+            dispatchGroup.enter()
+            if let uploadData = singleImage.pngData() {
+                storageRef.putData(uploadData, metadata: nil) { (metadata, error) in
+                    if error != nil {
+                        self.imagePathArray.append(error.debugDescription)
+                    } else {
+                        self.imagePathArray.append(imagePath.name)
+                    }
+                    dispatchGroup.leave()
+                }
             }
         }
-        //return documentID
+        dispatchGroup.notify(queue: .main) {
+            completion(self.imagePathArray)
+        }
     }
 }
